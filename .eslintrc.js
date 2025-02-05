2320154eb742b1ecaea45ab3929a9d14481b5b5e
André Martins <5861617+andremof@users.noreply.github.com>
2025-02-05 11:10:05 -0300

    chore: added config files eslint, prettier and git

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier'
+    ],
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    ignorePatterns: ['dist', 'node_modules', 'coverage', '*.js'],
+  };
